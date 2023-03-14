d5cf0794e11905070cd93f1a02ab3e906af9be33
kevisen18 <kevisen.permal@linkeo.com>
2023-03-14 16:21:11 +0400

    update
    update

--- modal/contact.php
+++ modal/contact.php
@@ -1,17 +1,35 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $message = $_POST["message"];
-  
-  $to = "your-email@example.com";
-  $subject = "New Contact Form Submission";
-  $body = "Name: $name\nEmail: $email\nMessage: $message";
-  
-  if (mail($to, $subject, $body)) {
-    echo "<div class='returnmessage' data-success='Your message has been received, We will contact you soon.'></div>";
-  } else {
-    echo "<div class='returnmessage' data-error='There was an error sending your message. Please try again later.'></div>";
+    // Set the recipient email address here
+    $to = "^pkevisen@gmail.com";
+    // Collect the form data
+    $name = trim($_POST["name"]);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $telephone = trim($_POST["telephone"]);
+    $subject = trim($_POST["subject"]);
+    $message = trim($_POST["message"]);
+    // Check if all required fields are filled
+    if (!empty($name) && !empty($email) && !empty($telephone) && !empty($subject) && !empty($message)) {
+      // Set the email subject and message
+      $email_subject = "New contact form submission: $subject";
+      $email_message = "Name: $name\nEmail: $email\nTelephone: $telephone\n\nMessage:\n$message";
+      // Set the email headers
+      $headers = "From: $name <$email>\r\n";
+      $headers .= "Reply-To: $email\r\n";
+      // Send the email
+      if (mail($to, $email_subject, $email_message, $headers)) {
+        // Set success status and redirect to index.php
+        header("Location: index.php?status=success");
+        exit;
+      } else {
+        // Set failure status and redirect to index.php
+        header("Location: index.php?status=failure");
+        exit;
+      }
+    } else {
+      // Set failure status and redirect to index.php
+      header("Location: index.php?status=failure");
+      exit;
+    }
   }
-}
 ?>
